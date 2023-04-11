779e71f543c650d03d4f64019bac38f0d1dfecba
Shaneron <shaneron109@gmail.com>
2023-04-12 05:36:35 +0900

    文字置換ツールのJS追加

---
+++ tools/text-replace/js/replace.js
@@ -0,0 +1,50 @@
+window.addEventListener("DOMContentLoaded", (e) => {
+    const before_textarea = document.getElementById("before-text");
+    const after_textarea = document.getElementById("after-text");
+    const button_box = document.querySelector(".button-box");
+    const add_button = document.getElementById("add-button");
+    const del_button = document.getElementById("del-button");
+    let id_num = 1;
+
+    before_textarea.addEventListener("input", () => {
+        let replace_num = document.querySelectorAll(`[id^="before-replace"]`).length;
+        let replece_content = before_textarea.value;
+        for(let i = 0; i < replace_num; i++){
+            let before_replace = document.getElementById(`before-replace-${i}`).value;
+            let after_replace = document.getElementById(`after-replace-${i}`).value;
+            replece_content = replece_content.replaceAll(before_replace, after_replace);
+        }
+        after_textarea.value = replece_content;
+    });
+
+    add_button.addEventListener("click", () => {
+        let replace_box = document.createElement("div");
+        let before_content = document.createElement("input");
+        let after_content = document.createElement("input");
+
+        replace_box.classList.add("replace-box")
+
+        before_content.type = "text";
+        before_content.setAttribute("id", `before-replace-${id_num}`);
+        after_content.type = "text";
+        after_content.setAttribute("id", `after-replace-${id_num}`);
+        id_num += 1;
+
+        let arrow = document.createElement("span");
+        let arrow_f = document.createTextNode("→");
+        arrow.appendChild(arrow_f);
+
+        replace_box.appendChild(before_content);
+        replace_box.appendChild(arrow);
+        replace_box.appendChild(after_content);
+
+        button_box.before(replace_box);
+    });
+
+    del_button.addEventListener("click", () => {
+        if(document.querySelector(".replace-container").childElementCount > 2){
+            button_box.previousElementSibling.remove();
+            id_num -= 1;
+        }
+    });
+});

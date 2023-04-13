84c79ea9b66e1af265f328dfef9442671be36d7c
DESKTOP-GF219SI\moti <lunaticnight00@gmail.com>
2023-04-14 05:46:04 +0900

    ツールのタイトル文字のリンクが適用されていなかったので修正

--- parts/main.php
+++ parts/main.php
@@ -9,12 +9,12 @@
 		<?for($i =1;$i <= $blocknum;$i++){?>
 			
 				<div class="post-b s1">
-					<a href="<?=$tools[$i- 1]['url']?>" aria-label='<?=$tools[$i- 1]['title']?>'>
+					<a href="<?=$tools[$i - 1]['url']?>" aria-label='<?=$tools[$i - 1]['title']?>'>
 						<article>
 							<div class="post-thumbnail" style="background-image:url(<?=$tools[$i- 1]['img']?>);"></div>
 						</article>
 					</a>
-					<div class="post-t"><a href="url"><h2><?=$tools[$i- 1]['title']?></h2><p><?=$tools[$i- 1]['description']?></p></a></div></div>
+					<div class="post-t"><a href="<?=$tools[$i - 1]['url']?>" aria-label='<?=$tools[$i - 1]['title']?>'><h2><?=$tools[$i- 1]['title']?></h2><p><?=$tools[$i- 1]['description']?></p></a></div></div>
 		<?}?>
 	</div>
 </main>

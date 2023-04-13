b4f48a90791276135ae915d15c11de0c077f2a77
DESKTOP-GF219SI\moti <lunaticnight00@gmail.com>
2023-04-14 05:34:23 +0900

    テキストファイル内容確認ツールのデータを追加

--- data.php
+++ data.php
@@ -4,5 +4,37 @@ $blog_info = [
     'description' => '',
     'home' => 'tools.motisan.info',
     'top-nav-txt' => 'もちもち便利ツール',
+    'header-img'=>'./i/header.png.webp',
+    'top-h2'=>'うおおおおおおおおおおおお！',
+];
+$tools = [
+    [
+        'title'=>'テキストファイル内容確認ツール',
+        'url'=>'./tools/show-text/',
+        'user'=>'',
+        'description'=>'Web上でファイル内の文字列を確認するツールです。',
+        'img'=>'',
+        'date'=>'2023/04/14',
+        'var'=>'1.0',
+        'id'=>'show-text'
+    ],
+    [
+        'title'=>'',
+        'url'=>'',
+        'user'=>'',
+        'description'=>'',
+        'img'=>'',
+        'date'=>'',
+        'var'=>'',
+    ],
+    /*[
+        'title'=>'',
+        'url'=>'',
+        'user'=>'',
+        'description'=>'',
+        'img'=>'',
+        'date'=>'',
+        'var'=>'',
+    ]*/
 ];
 ?>

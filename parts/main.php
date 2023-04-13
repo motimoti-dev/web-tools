40e533136ddae7ffb296516fa68da7956fe9cd15
DESKTOP-GF219SI\moti <lunaticnight00@gmail.com>
2023-04-14 06:18:05 +0900

    コード整形

--- parts/main.php
+++ parts/main.php
@@ -1,20 +1,24 @@
-<main><?
-	$blocknum = count($tools);
-    include( 'parts/share-pc.php' )?>
-	
+<main>
+    <?include( 'parts/share-pc.php' )?>
 	<div class="pbox">
 		<div style="grid-column: 1/3;">
 			<h2 class='top-h2'><?=$blog_info['top-h2']?></h2>
 		</div>
-		<?for($i =1;$i <= $blocknum;$i++){?>
-			
-				<div class="post-b s1">
+		<?$blocknum = count($tools);
+		for($i =1;$i <= $blocknum;$i++){?>
+			<div class="post-b s1">
+				<a href="<?=$tools[$i - 1]['url']?>" aria-label='<?=$tools[$i - 1]['title']?>'>
+					<article>
+						<div class="post-thumbnail" style="background-image:url(<?=$tools[$i- 1]['img']?>);"></div>
+					</article>
+				</a>
+				<div class="post-t">
 					<a href="<?=$tools[$i - 1]['url']?>" aria-label='<?=$tools[$i - 1]['title']?>'>
-						<article>
-							<div class="post-thumbnail" style="background-image:url(<?=$tools[$i- 1]['img']?>);"></div>
-						</article>
-					</a>
-					<div class="post-t"><a href="<?=$tools[$i - 1]['url']?>" aria-label='<?=$tools[$i - 1]['title']?>'><h2><?=$tools[$i- 1]['title']?></h2><p><?=$tools[$i- 1]['description']?></p></a></div></div>
+					<h2><?=$tools[$i- 1]['title']?></h2>
+					<p><?=$tools[$i- 1]['description']?></p>
+				</a>
+				</div>
+			</div>
 		<?}?>
 	</div>
 </main>

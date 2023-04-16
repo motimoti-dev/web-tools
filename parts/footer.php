9c2be13e2bb645ecaba5a3bc70f2c71027d46dc9
DESKTOP-GF219SI\moti <lunaticnight00@gmail.com>
2023-04-17 02:34:59 +0900

    フッター修正

--- parts/footer.php
+++ parts/footer.php
@@ -1,7 +1,7 @@
 <footer style="width:100%;text-align:center;"><?
 	?><a href="/" style="margin:10px 0" class='cdg fw7 dib tdn' area-lavel='トップへ'>トップへ</a><?
    ?><div style="padding: 4px 0  10px; 0" class='bgw'><?
-   	?><a style="font-size:10px;" href='//motisan.info/privacy-policy/' class='cg' area-lavel='利用規約'>利用規約</a><?
-   	?><p style="font-size:8px;" class='m0 cg'>@Copyright 2022-<?=date('Y')?>Webもち屋さん All rights reserved</p><?
+   	?><a style="font-size:10px;" href='./privacy-policy/' class='cg' area-lavel='利用規約'>利用規約</a><?
+   	?><p style="font-size:8px;" class='m0 cg'>@Copyright 2022-<?=date('Y')?><?=$blog_info['name']?> All rights reserved</p><?
    ?></div><?
 ?></footer>

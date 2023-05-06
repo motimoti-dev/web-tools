546263525ebec28b72dd277236311c1e9a184bf3
DESKTOP-GF219SI\moti <lunaticnight00@gmail.com>
2023-05-07 04:02:07 +0900

    dataのviewに対応して表示非表示を切り替え可能に

--- parts/main.php
+++ parts/main.php
@@ -6,19 +6,21 @@
 		</div>
 		<?$blocknum = count($tools);
 		for($i =1;$i <= $blocknum;$i++){?>
-			<div class="post-b s1">
-				<a href="<?=$tools[$i - 1]['url']?>" aria-label='<?=$tools[$i - 1]['title']?>'>
-					<article>
-						<div class="post-thumbnail" style="background-image:url(<?=$blog_info['base']?><?=$tools[$i - 1]['img']?>);"></div>
-					</article>
-				</a>
-				<div class="post-t">
+			<?if($tools[$i - 1]['view']){?>
+				<div class="post-b s1">
 					<a href="<?=$tools[$i - 1]['url']?>" aria-label='<?=$tools[$i - 1]['title']?>'>
-					<h2><?=$tools[$i- 1]['title']?></h2>
-					<p><?=$tools[$i- 1]['description']?></p>
-				</a>
+						<article>
+							<div class="post-thumbnail" style="background-image:url(<?=$blog_info['base']?><?=$tools[$i - 1]['img']?>);"></div>
+						</article>
+					</a>
+					<div class="post-t">
+						<a href="<?=$tools[$i - 1]['url']?>" aria-label='<?=$tools[$i - 1]['title']?>'>
+						<h2><?=$tools[$i- 1]['title']?></h2>
+						<p><?=$tools[$i- 1]['description']?></p>
+					</a>
+					</div>
 				</div>
-			</div>
+			<?}?>
 		<?}?>
 	</div>
 </main>

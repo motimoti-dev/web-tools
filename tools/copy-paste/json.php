247bd9c63d97a994747ec52ee4aa674050023202
DESKTOP-GF219SI\moti <lunaticnight00@gmail.com>
2023-06-12 09:11:44 +0900

    jsonデータをかき出すやつ

--- tools/copy-paste/json.php
+++ tools/copy-paste/json.php
@@ -3,6 +3,20 @@
 header("Content-type: application/x-javascript");
 $json = file_get_contents("https://api.motisan.info/json/data.json");
 $arr = json_decode($json,TRUE);
+print_r($arr);
+foreach($arr as $data){
+    if($data['use'] != ''){
+    ?><div>
+        <h2><?=addslashes($data['title'])?></h2>
+        <pre>
+            <code><?=addslashes($data['html'])?></code>
+        </pre>
+      </div>
+    <?
+    }
+}
+
+
 echo "document.write('<p>aa".$json."</p>');";
 
 ?>

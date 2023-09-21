bbaf26bfec2b3e2b590c1cba32a5670c20cc0e55
DESKTOP-GF219SI\moti <lunaticnight00@gmail.com>
2023-09-22 08:14:22 +0900

    canonicalがミスってるので直した

--- data.php
+++ data.php
@@ -20,7 +20,7 @@ $example = [
     [
         'title'=>'テストツール',
         'url'=>'./tools/example/',
-        'canonical'=>'https://motisan.info/tools/example/',
+        'canonical'=>'https://tools.motisan.info/tools/example/',
         'user'=>'',
         'description'=>'テストのツールです',
         'img'=>'i/example.png',
@@ -34,7 +34,7 @@ $tools = [
     [
         'title'=>'テキストファイル内容確認ツール',
         'url'=>'./tools/show-text/',
-        'canonical'=>'https://motisan.info/tools/show-text/',
+        'canonical'=>'https://tools.motisan.info/tools/show-text/',
         'user'=>'',
         'description'=>'Web上でファイル内の文字列を確認するツールです！',
         'img'=>'i/show-text.png',
@@ -46,7 +46,7 @@ $tools = [
     [
         'title'=>'文字置換ツール',
         'url'=>'./tools/text-replace/',
-        'canonical'=>'https://motisan.info/tools/text-replace/',
+        'canonical'=>'https://tools.motisan.info/tools/text-replace/',
         'user'=>'',
         'description'=>'指定した文字を一括で置き換えるツールです！',
         'img'=>'i/text-replace.png',
@@ -58,7 +58,7 @@ $tools = [
     [
         'title'=>'予定入りカレンダー生成',
         'url'=>'./tools/calendar-generator/output.php',
-        'canonical'=>'https://motisan.info/tools/calendar-generator/output.php',
+        'canonical'=>'https://tools.motisan.info/tools/calendar-generator/output.php',
         'user'=>'',
         'description'=>'文字やアイコンの入ったカレンダーを生成できます！',
         'img'=>'i/calendar-generator.png',
@@ -70,7 +70,7 @@ $tools = [
     [
         'title'=>'もちイラスト博物館',
         'url'=>'./tools/src-illust/',
-        'canonical'=>'https://motisan.info/tools/src-illust/',
+        'canonical'=>'https://tools.motisan.info/tools/src-illust/',
         'user'=>'',
         'description'=>'イラストが検索できます！',
         'img'=>'i/src-illust.png',
@@ -82,7 +82,7 @@ $tools = [
     [
         'title'=>'メタタグジェネレーター',
         'url'=>'./tools/meta-tag-generator/',
-        'canonical'=>'https://motisan.info/tools/meta-tag-generator/',
+        'canonical'=>'https://tools.motisan.info/tools/meta-tag-generator/',
         'user'=>'',
         'description'=>'HTMLのメタタグを作成できます',
         'img'=>'i/meta-tag-generator.png',
@@ -94,7 +94,7 @@ $tools = [
     [
         'title'=>'もち的コピペ集',
         'url'=>'./tools/copy-paste/output.php',
-        'canonical'=>'https://motisan.info/tools/copy-paste/output.php',
+        'canonical'=>'https://tools.motisan.info/tools/copy-paste/output.php',
         'user'=>'',
         'description'=>'もちが個人的によく使う文字列をコピペできます',
         'img'=>'i/copy-paste.png',
@@ -118,7 +118,7 @@ $tools = [
     [
         'title'=>'時間管理',
         'url'=>'./tools/time-manage/output.php',
-        'canonical'=>'https://motisan.info/tools/time-manage/output.php',
+        'canonical'=>'https://tools.motisan.info/tools/time-manage/output.php',
         'user'=>'',
         'description'=>'テスト運用中、新規登録不可、まだ作っているところです！',
         'img'=>'i/time-manage.png',
@@ -131,7 +131,7 @@ $tools = [
         //8
         'title'=>'SVGを横幅3000pxのPNGに変換するツール',
         'url'=>'./tools/svg2img/output.php',
-        'canonical'=>'https://motisan.info/tools/svg2img/output.php',
+        'canonical'=>'https://tools.motisan.info/tools/svg2img/output.php',
         'user'=>'',
         'description'=>'もち用',
         'img'=>'i/svg2png.png',

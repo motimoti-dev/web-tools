6fd16311001bc5065d0b00bdc2c3790f59c41539
DESKTOP-GF219SI\moti <lunaticnight00@gmail.com>
2023-10-07 06:11:52 +0900

    クリックでコピーの機能をつけた、暗号化のコード野途中

--- tools/crypter/encode.php
+++ tools/crypter/encode.php
@@ -30,6 +30,72 @@ date_default_timezone_set('Asia/Tokyo');?>
         <link rel="apple-touch-icon-precomposed" href="//motisan.info/i/apple.png">
         <meta name="msapplication-TileImage" content="//motisan.info/i/ms.png"><?//END共用部分?>
 
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/core.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/enc-base64.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/cipher-core.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/sha256.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/sha1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/aes.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/hmac.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/pbkdf2.min.js"></script>
+        <script>
+      // 暗号化
+      function encrypt(text, pass) {
+        // ソルト
+        const salt = CryptoJS.lib.WordArray.random(128 / 8);
+        // 初期ベクトル
+        const iv = CryptoJS.lib.WordArray.random(128 / 8);
+
+        // AESキーの生成(128bit、5万回)
+        const key = CryptoJS.PBKDF2(pass, salt, {
+          keySize: 256 / 32,
+          iterations: 100000,
+          hasher: CryptoJS.algo.SHA256,
+        });
+
+        // AESキーで暗号化
+        const encrypted = CryptoJS.AES.encrypt(text, key, {
+          iv: iv,
+        });
+        return {
+          salt: salt,
+          iv: iv,
+          encrypted: encrypted,
+        };
+      }
+
+      // 復号
+      function decrypt(encryptedData, pass) {
+        // AESキーの生成(256bit、10万回)
+        const key = CryptoJS.PBKDF2(pass, encryptedData.salt, {
+          keySize: 256 / 32,
+          iterations: 100000,
+          hasher: CryptoJS.algo.SHA256,
+        });
+
+        // AESキーで復号
+        const decrypted = CryptoJS.AES.decrypt(encryptedData.encrypted, key, {
+          iv: encryptedData.iv,
+        });
+
+        return decrypted.toString(CryptoJS.enc.Utf8);
+      }
+
+      const targetText = "暗号化したいテキスト";
+      const passPhrase = "password123";
+
+      // 暗号化
+      const encryptedData = encrypt(targetText, passPhrase);
+
+      // 復号
+      const decrypted = decrypt(encryptedData, passPhrase);
+
+      //暗号テキスト
+      console.log(encryptedData.encrypted.toString());
+      // 復号されたテキスト
+      console.log(decrypted.toString(CryptoJS.enc.Utf8));
+      
+    </script>
         <link rel="stylesheet" href="https://tools.motisan.info/css/index-style.css" http-equiv="Cache-Control" content="no-cache">
         <style>
         h1{
@@ -62,6 +128,7 @@ date_default_timezone_set('Asia/Tokyo');?>
         }
         h3{
             font-size:1.8rem;
+            color: #010079;text-shadow: 0 0 5px white;padding: 0.3em 0.5em;background: -webkit-repeating-linear-gradient(-45deg, #cce7ff, #cce7ff 3px,#e9f4ff 3px, #e9f4ff 7px);background: repeating-linear-gradient(-45deg, #cce7ff, #cce7ff 3px,#e9f4ff 3px, #e9f4ff 7px);
         }
         </style>
     </head>
@@ -72,7 +139,7 @@ date_default_timezone_set('Asia/Tokyo');?>
             <div class="wrapper center" style="margin-top:15px;padding-bottom:10px;max-width: 1350px;">
                 <main>
                     <iframe id="ShareIframe" class="ShareIframe" src="../../static/share.html" role="presentation" style="position: relative;left:-15px;width:50px;height:100%;float:left"></iframe>
-                    <section style="display: grid;max-width: 960px;background: white;padding: 20px;border-radius: 12px;" class="s1">
+                    <section style="display:grid;max-width:960px;background:white;padding:20px;border-radius:12px" class="s1">
                         <h1>文字暗号化ツール</h1>
                         <p style="padding: 15px;border: #ffd07b dashed 2px;background: #fefff8;color: #a7721a;border-radius: 10px;">このツールはテキストをパスフレーズを使って暗号化できるツールです。<br>暗号化後に本サービスが終了したとしても同じライブラリを使うことによって復号が可能です。<br>ただしここで使用している方法が「AES 256bit」であり、反復回数が何回であったかとパスフレーズを覚えておく必要があります。</p>
                         <div style="display: table;margin-bottom: 2em;width: 100%;max-width:700px;">
@@ -87,16 +154,31 @@ date_default_timezone_set('Asia/Tokyo');?>
                         
                         <h2>暗号化(AES 256bit)</h2>
                         <h3>暗号化したい文字</h3>
-                        <textarea style='max-width: 1200px;width: 100%;min-height: 200px;' id='target'></textarea>
+                        <textarea style='max-width:1200px;width: calc(100% - 2rem);min-height: 200px;margin-bottom:2rem;padding:1rem;border-radius: 2rem;font-size: 1.6rem;' id='target'></textarea>
                         <h3>パスフレーズ(日本語可)</h3>
-                        <input type='text' id='passphrase' name='passphrase'>
+                        <input type='text' id='passphrase' name='passphrase' style='margin-bottom: 2rem;padding: 1rem;border-radius: 2rem;font-size: 1.6rem;'>
                         <h3>反復回数</h3>
-                        <input type='number' value='100000' id='repetitions' name='repetitions' min="100000">
+                        <input type='number' value='100000' id='repetitions' name='repetitions' min="100000" style='margin-bottom: 2rem;padding: 1rem;border-radius: 2rem;font-size: 1.6rem;'>
                         <div>
                             <button onclick="" style="margin-bottom: 20px;font-size: 20px;width: calc(100%);border-radius: 100px;padding: 13px 20px;background: #ffd799;font-weight: 700;color: white;border: none;box-shadow: 2px 3px 6px #ff441e26;">出力する</button>
                         </div>
                         
+                        
+                        <div class='copy_target'>出力コード</div>                    
+                        <p class='op_copy'>クリックでコピー</p>
+                        <div class='copy_target2'>URLで共有</div>
+                        <p class='op_copy2'>クリックでコピー</p>
+                        <script>
+                        const COPY_BT = document.querySelector('.op_copy');
+                            COPY_BT.addEventListener('click', () =>{
+                            navigator.clipboard.writeText(document.querySelector('.copy_target').textContent);//クリップボードにコピー
+                        });
+                        const COPY_BT2 = document.querySelector('.op_copy2');
+                        COPY_BT2.addEventListener('click', () =>{
+		                    navigator.clipboard.writeText(document.querySelector('.copy_target2').textContent);//クリップボードにコピー
+	                    });
 
+                        </script>
                         <p>パスフレーズは日本語を含むと総当たりでの突破を困難にします！反復回数は増やせば増やすほどセキュリティが上がりますが数を大きくしすぎると端末の処理速度に依存して処理が重くなります。デフォルトでは100000回です。</p>
                         
                         <h2>注意</h2>
